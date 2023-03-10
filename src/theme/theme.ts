03d3d4f57af64a8abda2bf0df3082bbefb2ea71c
DEWITT SCOTT CHAVEZ PONCE <dschavezp@gmail.com>
2023-03-10 08:53:00 -0500

    Setting up project and styles

---
+++ src/theme/theme.ts
@@ -0,0 +1,81 @@
+export const theme = {
+    typography: {
+        display: {
+            xL: {
+                size: '64px',
+                lineHeight: '88px',
+                letterSpacing: '1px'
+            },
+            L: {
+                size: '56px',
+                lineHeight: '72px',
+                letterSpacing: '1px'
+            },
+            M: {
+                size: '48px',
+                lineHeight: '56px',
+                letterSpacing: '1px'
+            },
+            S: {
+                size: '32px',
+                lineHeight: '48px',
+                letterSpacing: '1px'
+            },
+            xS: {
+                size: '24px',
+                lineHeight: '32px',
+                letterSpacing: '1px'
+            }
+        },
+        body: {
+            xL: {
+                size: '20px',
+                lineHeight: '32px',
+                letterSpacing: '.75px'
+            },
+            L: {
+                size: '18px',
+                lineHeight: '32px',
+                letterSpacing: '.75px'
+            },
+            M: {
+                size: '15px',
+                lineHeight: '24px',
+                letterSpacing: '.75px'
+            },
+            S: {
+                size: '13px',
+                lineHeight: '22px',
+                letterSpacing: '.25px'
+            },
+        }
+    },
+    colors: {
+        neutral: {
+            '1': '#FFFFFF',
+            '2': '#94979A',
+            '3': '#393D41',
+            '4': '#2C2F33',
+            '5': '#222528',
+        },
+        primary: {
+            '1': '#F4CCC8',
+            '2': '#EBA59E',
+            '3': '#E27D73',
+            '4': '#DA584B',
+        },
+        secondary: {
+            '1': '#C8E1BC',
+            '2': '#AAD199',
+            '3': '#8DC275',
+            '4': '#70B252',
+        },
+        tertiary: {
+            '1': '#F9EED7',
+            '2': '#F2DAAB',
+            '3': '#EBC77F',
+            '4': '#E5B454',
+        }
+
+    }
+}
